f2dca16a6029a54542d909dde81bc28e96c7ff9c
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-31 23:13:30 +0900

    Add Comments: Question and Answer about Directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -18,12 +18,26 @@ export class DropdownDirective{
     constructor(private elementRef: ElementRef) {}
 
     @HostListener('document:click', ['$event']) toggleOpen(event:Event) {
-        console.log('[document:click]' + event.target);
         this.isOpen
          = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
 
     /**
+     * My Question List about this Directive.
+     * 
+     * Q1. At this code, i cannot understand the difference between ['$event'] as HostListener argument
+     * and event:Event in toggleOpen() argument.
+     * 1. What's the difference between the two components?
+     * 2. What's the purpose for?
+     * 3. and Why we need to setup like this?
+     * 
+     * A. If we use the event inside the function, we have to pass it in this way to the function (with this ugly array syntax, unfortunately)
+     * If we wouldn't use it in the function (like in the lecture before) we could simply write,
+     * 
+     * @HostListener('document:click') toggleOpen() { ... }
+     *
+     *
+     * StackOverflow: https://stackoverflow.com/questions/47156492
      * $event is the reserved name for the actual event value like it can be used in
      * (click) = "clickHandler($event)" event bindings
      * 
