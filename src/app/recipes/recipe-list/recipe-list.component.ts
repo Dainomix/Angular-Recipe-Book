8ce30c06308ac4bdb5de2534cef6062834adc63a
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-02 23:32:50 +0900

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,24 +11,12 @@ export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Reipe', 'This is simply a test', 
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
-    ),
-    new Recipe(
-      'Another Test Reipe', 'This is simply a test', 
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
-    ),
-    new Recipe(
-      'A Test Reipe 3', 'This is simply a test', 
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
-    ),
-  ]
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipe();
   }
 
   onRecipeSelected(recipe:Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,23 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
 
+    private recipes: Recipe[] = [
+        new Recipe(
+          'A Test Reipe', 'This is simply a test', 
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        ),
+        new Recipe(
+          'Another Test Reipe', 'This is simply a test', 
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        ),
+        new Recipe(
+          'A Test Reipe 3', 'This is simply a test', 
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        ),
+      ];
+    
+    getRecipe() {
+        return this.recipes.slice();    // return copy of recipe array.
+    }
 }
