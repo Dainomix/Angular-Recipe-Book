fa2fe1918fef289be06f3269dd2a6841cc3af4fd
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-21 23:10:06 +0900

    One Note about Route Observables

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -13,6 +13,18 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
 
+  /**
+   * We don't need to clean up the subscription here.
+   * 
+   * In other cases where we use our own observables, 
+   * observabls we created and not managed by Angular
+   * 
+   * Therefore, we will need to clean up the subscription.
+   * 
+   * So definitely keep that in mind if we work with our own observables, here for now.
+   * We can simply proceed with the set up and let Angular do the celanup work.
+   */
+
   ngOnInit(): void {
     this.route.params
       .subscribe(
